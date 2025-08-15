292db32dd93bdcb9c3fc4af33c3c0eb5b57b1bd6
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-15 15:48:25 -0300

    make script in batch

--- src/scripts/generate-audio-segments.py
+++ src/scripts/generate-audio-segments.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 from openai import OpenAI
+import json
 
 client = OpenAI()
-speech_file_path = Path(__file__).parent / "../.." / "articles" / "how-do-webhooks-work" / "audios" / "01.mp3"
 
-with client.audio.speech.with_streaming_response.create(
-        model="gpt-4o-mini-tts",
-        voice="onyx",
-        input="Once upon a time, there was an e‑commerce store. They used an external payment service to handle orders.",
-        instructions="Speak as if you were a teacher explaining technical content.",
-) as response:
-    response.stream_to_file(speech_file_path)
+articles_path = Path(__file__).parent / "../../articles"
+for f in articles_path.iterdir():
+    with open(articles_path / f.name / "transcripts/transcript.json", 'r') as file:
+        transcript = json.load(file)
+    for i, segment in enumerate(transcript['segments']):
+        speech_file_path = articles_path / f.name / "audios" / f"{i}.mp3"
+        with client.audio.speech.with_streaming_response.create(
+                model="gpt-4o-mini-tts",
+                voice="onyx",
+                input=' '.join(segment['sentences']),
+                instructions="Speak as if you were a teacher explaining technical content.",
+        ) as response:
+            response.stream_to_file(speech_file_path)
