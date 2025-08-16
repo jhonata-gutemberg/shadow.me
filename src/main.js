9676c0fe8caeafc8c21a5266dc65ca921baa928b
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-15 21:03:28 -0300

    add side menu

--- src/main.js
+++ src/main.js
@@ -1,40 +1,41 @@
-const header = document.querySelector("main")
 const main = document.querySelector("main")
 
-function showArticle(article) {
-    header.innerHTML = ""
+function showArticle(key) {
     main.innerHTML = ""
-    fetch(`./articles/${article}/transcripts/transcript.json`)
-      .then(res => res.json())
-      .then(transcript => {
-        const h1 = document.createElement("h1")
-        const h2 = document.createElement("h2")
-        h1.innerHTML = transcript.title
-        h2.innerHTML = transcript.subtitle
-        header.append(h1)
-        header.append(h2)
+    fetch(`./articles/${key}/transcripts/transcript.json`)
+        .then(res => res.json())
+        .then(transcript => {
+            const article = document.createElement("article")
+            const header = document.createElement("header")
+            const h1 = document.createElement("h1")
+            const h2 = document.createElement("h2")
+            h1.innerHTML = transcript.title
+            h2.innerHTML = transcript.subtitle
+            header.append(h1)
+            header.append(h2)
+            article.append(header)
 
-        const segments = transcript.segments
-        for (let i = 0; i < segments.length; i++) {
-            const segmentDiv = document.createElement("div")
-            const div = document.createElement("div")
-            const audio = document.createElement("audio")
-            const source = document.createElement("source")
+            const segments = transcript.segments
+            for (let i = 0; i < segments.length; i++) {
+                const segmentDiv = document.createElement("div")
+                const div = document.createElement("div")
+                const audio = document.createElement("audio")
+                const source = document.createElement("source")
 
-            const segment = segments[i]
-            segment.sentences.forEach(sentence => {
-                const p = document.createElement("p")
-                p.innerHTML = sentence
-                div.appendChild(p)
-            })
-            source.setAttribute("src", `./articles/${article}/audios/${i}.mp3`)
-            audio.setAttribute("controls", true)
-            audio.appendChild(source)
-            segmentDiv.className = "segment"
-            segmentDiv.appendChild(div)
-            segmentDiv.appendChild(audio)
-
-            main.appendChild(segmentDiv)
-        }
-      });
+                const segment = segments[i]
+                segment.sentences.forEach(sentence => {
+                    const p = document.createElement("p")
+                    p.innerHTML = sentence
+                    div.appendChild(p)
+                })
+                source.setAttribute("src", `./articles/${key}/audios/${i}.mp3`)
+                audio.setAttribute("controls", "true")
+                audio.appendChild(source)
+                segmentDiv.className = "segment"
+                segmentDiv.appendChild(div)
+                segmentDiv.appendChild(audio)
+                article.appendChild(segmentDiv)
+            }
+            main.append(article)
+        });
 }
